4dc0ac2ea96b641eaf899ec282d29f9a34bb1003
taeju-01 <taeju1476@gmail.com>
2026-01-13 20:32:58 +0900

    Title: 중앙값 계산 2, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260113/중앙값 계산 2/get-median-2.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int n;
+int arr[100];
+
+int main() {
+    cin >> n;
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+    
+    for(int i=0;i<n;i++){
+        if(i%2==0){
+            sort(arr,arr+i+1);
+            cout<<arr[(i/2)]<<" ";
+        }
+
+    }
+
+    // Please write your code here.
+
+    return 0;
+}
