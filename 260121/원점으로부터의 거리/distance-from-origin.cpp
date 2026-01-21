9877b677d38bfd7bfcd72b66375d2541fa92458a
taeju-01 <taeju1476@gmail.com>
2026-01-21 20:48:08 +0900

    Title: 원점으로부터의 거리, Time: 8ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 260121/원점으로부터의 거리/distance-from-origin.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int N;
+int x;
+int num;
+int y;
+class Distance{
+    public:
+    int x;
+    int y;
+    int num;
+
+    Distance(int x=0,int y=0,int num=0){
+        this->x=x;
+        this->y=y;
+        this->num=num;
+    }
+};
+
+bool cmp(Distance a,Distance b){
+    if(abs(a.x+a.y)!=abs(b.x+b.y))
+    return abs(a.x+a.y)<abs(b.x+b.y);
+    return a.num<b.num;
+}
+
+int main() {
+    cin >> N;
+    Distance position[N];
+
+    for (int i = 0; i < N; i++) {
+        cin >> x >> y;
+        position[i]=Distance(x,y,i+1);
+    }
+
+    sort(position,position+N,cmp);
+
+    for(int i=0;i<N;i++){
+        cout<<position[i].num<<endl;
+    }
+    
+
+    // Please write your code here.
+
+    return 0;
+}
